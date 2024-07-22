d0fb6763b62958a1224d56c06345fcb90cea4e75
ajmalsabith <ajmalsabith444@gmail.com>
2024-07-22 18:06:26 +0530

    second-api-invoice

--- mailSend/mailsend.js
+++ mailSend/mailsend.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const router = Router();
 const dotenv = require('dotenv').config();
 const nodemailer = require('nodemailer');
+const puppeteer = require('puppeteer');
 
 router.post('/sendmail', async (req, res) => {
     const { sub, mail, cc, attach, html } = req.body;
@@ -57,6 +58,66 @@ router.post('/sendmail', async (req, res) => {
                 });
             }
         });
+    }
+});
+
+router.post("/mail-invoice", async (req, res) => {
+    const { sub, mail, cc, html } = req.body;
+
+    try {
+        // console.log('Launching Puppeteer');
+        const browser = await puppeteer.launch({
+            headless: true,  // Ensure Puppeteer runs in headless mode
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
+        const page = await browser.newPage();
+
+        // Log console messages from the page
+        page.on('console', (msg) => console.log('PAGE LOG:', msg.text()));
+
+        // console.log('Setting content');
+        await page.setContent(html, { waitUntil: 'networkidle2', timeout: 120000 });
+
+        // console.log('Generating PDF');
+        const pdfBuffer = await page.pdf({ format: 'A4', timeout: 120000 });
+        await browser.close();
+
+        const attachArray = [{
+            filename: 'opsm.pdf',
+            content: pdfBuffer,
+        }];
+
+        const mailOptions = {
+            from: process.env.user,
+            to: Array.isArray(mail) ? mail.join(', ') : mail,
+            cc: Array.isArray(cc) ? cc.join(', ') : cc,
+            subject: sub,
+            html: '<p>Invoice.</p>',
+            attachments: attachArray,
+        };
+
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: process.env.user,
+                pass: process.env.pass,
+            },
+        });
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log('Error sending email:', error);
+                res.status(400).send({ message: "Email sending failed." });
+            } else {
+                console.log(`Email sent: ${info.response}`);
+                res.status(200).send({ message: "Email sent successfully." });
+            }
+        });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send({ message: "Internal Server Error" });
     }
 });
 
