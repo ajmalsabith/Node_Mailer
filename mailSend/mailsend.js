ad74ef5a5a4ecd0072f8b18b4ff7f2bd22d68a8f
ajmalsabith <ajmalsabith444@gmail.com>
2024-10-14 14:21:59 +0530

    opscrm-mail

--- mailSend/mailsend.js
+++ mailSend/mailsend.js
@@ -4,18 +4,26 @@ const dotenv = require('dotenv').config();
 const nodemailer = require('nodemailer');
 const puppeteer = require('puppeteer');
 const { google } = require('googleapis');
+const axios = require('axios');
 
 
+
+
+// gmail apis conf
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
 const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+
+
 
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
   CLIENT_SECRET,
   REDIRECT_URI
 );
+
+  
 
 oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
@@ -57,6 +65,95 @@ router.post('/sendmail', async (req, res) => {
                 attachments: attachArray,
             };
 
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    if (error.responseCode === 550 || error.response.includes("No such user")) {
+                        console.log('Email address does not exist');
+                        res.status(400).send({
+                            message: "Email address does not exist."
+                        });
+                    } else {
+                        console.log(error);
+                        res.status(400).send({
+                            message: "Email sending failed."
+                        });
+                    }
+                } else {
+                    console.log(`Email sent: ${info.response}`);
+                    res.status(200).send({
+                        message: "Email sent successfully."
+                    });
+                }
+            });
+
+        } catch (error) {
+            console.log('Error generating access token', error);
+            res.status(500).send({
+                message: "Failed to send email."
+            });
+        }
+    }
+});
+
+
+
+
+const CLIENT_OPSCRM_ID = process.env.OPSCRM_CLIENTID;
+const CLIENT_OPSCRM_SECRET = process.env.OPSCRM_CLIENT_SECRET;
+const REFRESH_OPSCRM_TOKEN = process.env.OPSCRM_REFRESH_TOKEN;
+
+
+const oAuth2ClientOPSCRM = new google.auth.OAuth2(
+    CLIENT_OPSCRM_ID,
+    CLIENT_OPSCRM_SECRET,
+    REDIRECT_URI
+  );
+
+  
+oAuth2ClientOPSCRM.setCredentials({ refresh_token: REFRESH_OPSCRM_TOKEN });
+
+
+
+router.post('/sendmail-opscrm', async (req, res) => {
+    const { sub, mail, cc, attach, html } = req.body;
+    const attachArray = [];
+    if (req.body) {
+        attach.forEach(element => {
+            const base64String = element.filestring.split(',')[1];
+            const obj = {
+                filename: element.filename,
+                content: base64String,
+                encoding: 'base64',
+            };
+            attachArray.push(obj);
+        });
+
+        try {
+            const accessToken = await oAuth2ClientOPSCRM.getAccessToken();
+
+            let transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    type: 'OAuth2',
+                    user: process.env.OPMSCRM_EMAIL,
+                    clientId: CLIENT_OPSCRM_ID,
+                    clientSecret: CLIENT_OPSCRM_SECRET,
+                    refreshToken: REFRESH_OPSCRM_TOKEN,
+                    accessToken: accessToken.token,
+                },
+            });
+
+            const mailOptions = {
+                from: process.env.OPMSCRM_EMAIL,
+                to: Array.isArray(mail) ? mail.join(', ') : mail,
+                cc: Array.isArray(cc) ? cc.join(', ') : cc,
+                subject: sub,
+                html: html,
+                attachments: attachArray,
+            };
+
+            console.log(mailOptions);
+            
             transporter.sendMail(mailOptions, (error, info) => {
                 if (error) {
                     if (error.responseCode === 550 || error.response.includes("No such user")) {
@@ -208,5 +305,72 @@ router.post("/mail-invoice", async (req, res) => {
         res.status(500).send({ message: "Internal Server Error" });
     }
 });
+
+
+
+
+// router.post('/send-mail-microsoft', async (req, res) => {
+//     const { sub, mail, cc, attach, html } = req.body; 
+//     const senderEmail = 'noreply@company.com'; // Shared mailbox or user mailbox
+
+//     // Fetch access token for Microsoft Graph API
+//     const accessToken = await getAccessToken();
+    
+//     if (!accessToken) {
+//         console.error("No access token available");
+//         return res.status(500).send('Access token not available');
+//     }
+
+//     // Construct toRecipients and ccRecipients from arrays
+//     const toRecipients = mail.map(email => ({
+//         emailAddress: { address: email }
+//     }));
+    
+//     const ccRecipients = cc ? cc.map(email => ({
+//         emailAddress: { address: email }
+//     })) : [];
+
+//     // Prepare attachments in Base64 format
+//     const attachments = attach.map(file => ({
+//         "@odata.type": "#microsoft.graph.fileAttachment",
+//         name: file.name, // Filename of the attachment
+//         contentBytes: file.contentBytes, // Base64-encoded content
+//         contentType: file.contentType, // Content type (e.g., "application/pdf")
+//     }));
+
+//     const emailData = {
+//         message: {
+//             subject: sub,
+//             body: {
+//                 contentType: "HTML",
+//                 content: html || "This is a test email sent from Node.js using Microsoft Graph API!"
+//             },
+//             toRecipients: toRecipients,
+//             ccRecipients: ccRecipients,
+//             attachments: attachments,
+//         },
+//         saveToSentItems: "true"
+//     };
+
+//     try {
+//         // Send the email using Microsoft Graph API
+//         const response = await axios.post(
+//             `https://graph.microsoft.com/v1.0/users/${encodeURIComponent(senderEmail)}/sendMail`, // Use dynamic sender email
+//             { message: emailData.message, saveToSentItems: "true" },
+//             {
+//                 headers: {
+//                     Authorization: `Bearer ${accessToken}`,
+//                     'Content-Type': 'application/json'
+//                 }
+//             }
+//         );
+//         console.log('Email sent successfully:', response.data);
+//         res.status(200).send('Email sent successfully');
+//     } catch (error) {
+//         console.error("Error sending email:", error.response ? error.response.data : error.message);
+//         res.status(500).send(error.response ? error.response.data : error.message);
+//     }
+// });
+
 
 module.exports = router;
